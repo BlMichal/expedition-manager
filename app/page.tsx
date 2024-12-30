74079597c30d7aa5caf1d1d30033ea93afbf903b
Michal Blažek <blaza0@email.cz>
2024-12-30 10:05:22 +0100

    v0.1.1 form

--- app/page.tsx
+++ app/page.tsx
@@ -73,12 +73,22 @@ export default function Home() {
         ))}
       </div>
       <form onSubmit={handleSubmit} className="mb-6 flex flex-col gap-4 bg-gray-400 p-4 ml-10 rounded max-w-96">
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          className="p-2 border rounded"
-        />
+        <div className="relative z-0 w-full group">
+          <input
+            type="text"
+            name="title"
+            id="title"
+            className="block px-2 py-2.5 w-full text-sm text-black bg-transparent bg-white border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-orange-500 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="title"
+            className="ml-2 px-1 peer-focus:font-medium absolute text-sm text-black rounded-lg bg-white peer-focus:bg-orange-600 dark:text-gray-400 duration-300 transform -translate-y-4 scale-85 top-2 z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-black peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-85 peer-focus:-translate-y-4"
+          >
+            Název
+          </label>
+        </div>
         <label className="flex items-center gap-2 text-black">
           <input type="checkbox" name="stackable" />
           Stackable
@@ -91,22 +101,7 @@ export default function Home() {
             className="p-2 border rounded"
           />
         </label>
-        <div className="relative z-0 w-full group">
-          <input
-            type="text"
-            name="title"
-            id="title"
-            className="block py-2.5 w-full text-sm text-black bg-transparent bg-white border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-orange-500 focus:outline-none focus:ring-0 focus:border-orange-600 peer"
-            placeholder=" "
-            required
-          />
-          <label
-            htmlFor="title"
-            className="ml-2 px-1 peer-focus:font-medium absolute text-sm text-black rounded-lg bg-white peer-focus:bg-orange-600 dark:text-gray-400 duration-300 transform -translate-y-5 scale-85 top-2 z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-black peer-focus:dark:text-orange-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-85 peer-focus:-translate-y-5"
-          >
-            Název
-          </label>
-        </div>
+
         <input
           type="number"
           name="width"
